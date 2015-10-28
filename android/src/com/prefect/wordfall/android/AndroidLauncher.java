0f78ed5a1ee6cc38ea04f51bb81a8139660d58ce
Daniel Brown <danjenator@gmail.com>
2015-10-28 16:31:27 -0500

    og push

---
+++ android/src/com/prefect/wordfall/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.prefect.wordfall.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.prefect.wordfall.WordFallGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WordFallGame(), config);
+	}
+}
---
+++ desktop/src/com/prefect/wordfall/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.prefect.wordfall.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.prefect.wordfall.WordFallGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WordFallGame(), config);
+	}
+}
