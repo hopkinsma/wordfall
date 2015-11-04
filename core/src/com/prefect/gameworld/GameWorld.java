933a56406db7a29e54bfe7c1d4cf29503a55b72e
mikey05 <mahgdd@gmail.com>
2015-11-04 12:57:57 -0600

    Added GameButton class.

--- core/src/com/prefect/gameworld/GameWorld.java
+++ core/src/com/prefect/gameworld/GameWorld.java
@@ -3,8 +3,14 @@ package com.prefect.gameworld;
 
 public class GameWorld {
 	
+	public FallingLetter gameLetters[];
+	public Player thePlayer;
+	public GameButton startButton;
+	
 	public GameWorld(int midPointY){
-		
+		gameLetters = new FallingLetter[6];
+		thePlayer = new Player("Player 1");
+		startButton = new GameButton(20, 20, 100, 20);
 	}
 
 	public void update(float delta) {
